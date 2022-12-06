2494c91e43d7f47092518cdebf10d3f666ad6033
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-12-06 12:35:36 +1100

    Update apps

---
+++ apps/bob_ross/07_bob_ross/global.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+library(echarts4r)
+
+# Tidy the data
+elements <- read_csv("data/elements-by-episode.csv") %>% 
+  separate(EPISODE, c("season", "episode"), sep = c(3,6)) %>% 
+  gather("element", "exists", -season, -episode, -TITLE) %>% 
+  mutate(
+    season = parse_number(season),
+    episode = parse_number(episode),
+    element = str_to_sentence(str_replace_all(element, "_", " ")),
+    TITLE = str_to_sentence(TITLE),
+    exists = as.logical(exists)
+  ) %>% 
+  rename(title = TITLE)
